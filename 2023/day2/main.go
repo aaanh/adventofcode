720c97978b5a864c47c6d8027820d66182491d15
Anh Hoang Nguyen <AnhNguyen@aaanh.com>
2023-12-04 00:00:59 -0500

    Defer until after exams :)

--- 2023/day2/main.go
+++ 2023/day2/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -31,6 +33,17 @@ func parseLineToGame(line string) game {
 
 	configs := strings.Split(string(strings.Split(string(line), ":")[1]), ";")
 
+	fmt.Print(configs, " ")
+
+	re := regexp.MustCompile(`\d+|\ |red|green|blue`)
+
+	for _, config := range configs {
+		for _, set := range strings.Split(config, ",") {
+			matches := re.FindAllString(set, -1)
+			fmt.Println(matches)
+		}
+	}
+
 	return g
 
 }
@@ -42,5 +55,8 @@ func main() {
 
 	input, _ := os.ReadFile("./day2.input")
 	lines := strings.Split(string(input), "\n")
+	for _, line := range lines {
+		parseLineToGame(line)
+	}
 
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module aaanh/adventofcode
+
+[dependencies]
