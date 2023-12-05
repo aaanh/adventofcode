dba5a9591fd1a1ec6003baada4628ed60bd7f8c3
Anh Nguyen <anhnguyen@aaanh.com>
2023-12-05 14:08:54 -0500

    Day 4 WIP

--- 2023/day4/main.go
+++ 2023/day4/main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
+	// "strconv"
 	"strings"
 )
+
+type Card struct {
+	Id       int
+	Winnings []int
+	Draws    []int
+}
+
+func parseLineToCard(line string) Card {
+	spli
+}
 
 func main() {
 	input, _ := os.ReadFile("./input")
 	lines := strings.Split(string(input), "\n")
-
+	for _, line := range lines {
+		fmt.Println(line)
+	}
 }
